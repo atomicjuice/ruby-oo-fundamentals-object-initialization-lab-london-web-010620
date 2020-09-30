9ca29f3749bfe18351e7eb104368a7fd16e66690
keemo watson <keemowatson1@gmail.com>
2020-09-30 20:43:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
 
   def initialize(name, breed)
+    @name = name
+    @breed = breed
     def check
       if breed = nil |@breed = "Mutt"|
     end
-    @name = name
-    @breed = breed
   end
 
 
